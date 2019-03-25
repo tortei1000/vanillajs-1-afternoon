ed97361926b1e7c3596519fc9c3050bf22dba132
tiago maffei <tiagomaffei@gmail.com>
2019-03-25 16:56:40 -0600

    all done

--- Part1/index.js
+++ Part1/index.js
@@ -15,52 +15,53 @@ function play(clickedId) {
   let playerSpan = document.getElementById("player")
   
   let clickedElement = document.getElementById(clickedId)
-  if (playerSpan.innerText === 'X') {
-    playerSpan.innerText = 'O'
-    clickedElement.innerText = 'X'
-    board[clickedId] = 'X'
-  }
-  else {
+  if(!clickedElement.innerText){
+  
+    if(playerSpan.innerText === 'X') {
+      playerSpan.innerText = 'O'
+      clickedElement.innerText = 'X'
+      board[clickedId] = 'X'
+    } else{
     playerSpan.innerText = 'X'
     clickedElement.innerText = 'O'
     board[clickedId] = 'O'
-
+   }
   }
-  
-
-
-  // if (topRight !== undefined && topRight === topCenter && topRight === topLeft){
-  //   alert(`${topRight} is the winner`);
-
-  // }
 
   if (board[0] !== undefined && board[0] === board[1] && board[0] === board[2]) {
-    alert(`${board[0]} is the winmner`)
+    alert(`${board[0]} is the winner`)
+    resetGame()
     return
   }
 
   if (board[0] !== undefined && board[0] === board[4] && board[0] === board[8]) {
-    alert(`${board[0]} is the winmner`)
+    alert(`${board[0]} is the winner`)
+    resetGame()
     return
   }
   if (board[0] !== undefined && board[0] === board[3] && board[0] === board[6]) {
-    alert(`${board[0]} is the winmner`)
+    alert(`${board[0]} is the winner`)
+    resetGame()
     return
   }
   if (board[1] !== undefined && board[1] === board[4] && board[1] === board[7]) {
-    alert(`${board[1]} is the winmner`)
+    alert(`${board[1]} is the winner`)
+    resetGame()
     return
   }
   if (board[2] !== undefined && board[2] === board[4] && board[2] === board[6]) {
-    alert(`${board[2]} is the winmner`)
+    alert(`${board[2]} is the winner`)
+    resetGame()
     return
   }
   if (board[2] !== undefined && board[2] === board[5] && board[2] === board[8]) {
-    alert(`${board[0]} is the winmner`)
+    alert(`${board[2]} is the winner`)
+    resetGame()
     return
   }
   if (board[8] !== undefined && board[8] === board[7] && board[8] === board[6]) {
-    alert(`${board[8]} is the winmner`)
+    alert(`${board[8]} is the winner`)
+    resetGame()
     return
   }
 
@@ -75,4 +76,14 @@ function play(clickedId) {
   if (boardFull === true) {
     alert("it is a tie")
   }
+}
+
+function resetGame(){
+  board.splice(0,8)
+  console.log(board)  
+  let resetId = document.getElementsByTagName('td') 
+  for(let i = 0; i < resetId.length; i++ ){
+    resetId[i].innerText = ''
+  }
+  console.log(document.getElementsByTagName('td')[0])
 }
